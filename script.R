3f681ecdd7a9b5debdc1933ccedf4a0660cadaad
Jun He <junnhe@gmail.com>
2013-04-23 10:47:12 -0600

    prototype of fill.frame is done

--- script.R
+++ script.R
@@ -28,3 +28,57 @@ play_movie <- function(df, mypid, save.pic=F)
 play_movie(df, 53)
 
 
+
+fill.frame <- function(df)
+{
+  # Sort by time, then by logical offset
+  df = df[with(df, order(Begin_timestamp, Logical_offset)), ]
+  df$ORG.PID = as.factor(df$ORG.PID)
+  df$WriteID = row.names(df)
+  
+  
+  fend = max(df$Logical_tail, na.rm=T)
+  frame.length = ceiling(sqrt(fend))
+  print( c("frame.length:", frame.length) )
+  
+  p <- ggplot(df, aes()) + ylim(c(0, frame.length)) + xlim(c(0, frame.length)) 
+  p
+  
+  
+  n = nrow(df)
+  for ( i in 1:n ) {
+    entry = df[i,]
+	print(entry)
+    # sx: segment x
+    # rx: row x
+    sx = entry$Logical_offset %% frame.length
+    sxend = entry$Logical_tail %% frame.length
+    sy = floor(entry$Logical_offset / frame.length)
+    syend = floor(entry$Logical_tail / frame.length)
+    print( c("sx, sxend, sy, syend", sx, sxend, sy, syend) )
+	
+	df.seg = data.frame(rx=NULL,rxend=NULL,ry=NULL,ryend=NULL)
+	
+    for (yi in sy:syend) {
+      ry = yi
+      ryend = yi
+      
+      rx = 0
+      rxend = frame.length
+      if ( yi == sy ) {
+        # The starting row
+        rx = sx
+      } else if (yi == syend) {
+        # The ending row
+        rxend = sxend        
+      }
+      df.seg = rbind(df.seg, c(rx,rxend,ry,ryend))
+    }
+	names(df.seg) = c("rx","rxend","ry","ryend")
+	newp <- last_plot() + 
+            geom_segment(data=df.seg, aes(x=rx, y=ry, xend=rxend, yend=ryend))
+    print(newp)	
+  }
+}
+fill.frame(df)
+
