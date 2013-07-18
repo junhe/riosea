c63e801ad17f3b0948483cfb73484315ffb3b18b
Jun He <junnhe@gmail.com>
2013-07-18 17:25:58 -0500

    map_stat() now has basic functions

--- statvis.R
+++ statvis.R
@@ -70,30 +70,52 @@ plot_gantt <- function (df)
 # Length statistics
 map_stat <- function(df)
 {
-	unitdiv = 1024 #show KB results
+	stime = min(df$Begin_timestamp)
+	df$Begin_timestamp = df$Begin_timestamp - stime
+	df$End_timestamp = df$End_timestamp - stime
+	
 	
 	df$duration = df$End_timestamp - df$Begin_timestamp
 	
+	
+	pfileview = plot_gantt_plfs_fileview_sort(df)
+	windows()
+	print(pfileview)
+	
+	
+	pgannt = plot_gantt_plfs(df)
+	windows()
+	print(pgannt)
+	
+	
 	print("Write range")
-	print(c(min(df$Logical_offset)/unitdiv, 
-	        max(df$Logical_tail)/unitdiv) )
+	print(paste(min(df$Logical_offset), " to ",
+	        max(df$Logical_tail)) )
 	
 	print("Overall duation time")
-	print( 
-			max(df$End_timestamp)-min(df$Begin_timestamp)
-		 )
+	overalltime = max(df$End_timestamp)-min(df$Begin_timestamp)
+	print( overalltime )
 	
-	print("Total number of writes (may not be accurate due to merging)")
+	print("Aggragated write size")
+	overallsize = sum(as.numeric(df$Length))
+	print( paste(overallsize, "bytes or ", overallsize/(1024*1024), "MB") )
+	
+	print("Overall bandwidth (may be lower than peak)")
+	overallbw = overallsize/overalltime
+	print( paste(overallbw/(1024*1024), "MB/s") )
+	
+	print("Total number of writes (may not be accurate due to merging before tracing)")
 	print(nrow(df))
 	
 	print("Length summary 1")
 	print(summary(df$Length))
 	
-	print("Length summary 2")
-	print(summary(as.factor(df$Length)))
+	#print("Length summary 2")
+	#print(summary(as.factor(df$Length)))
 	
 	phist_len = ggplot(df, aes(Length)) +
 			geom_histogram(aes(y  =  ..count..), binwidth  =  1024)
+	windows()
 	print(phist_len)
 	
 	
@@ -101,12 +123,102 @@ map_stat <- function(df)
 			geom_histogram(aes(y  =  ..count..))
 	windows()
 	print(phist_duration)
+	
+	# Holes and overlaps
+	df = find_hole(df)
+	print("Number of holes")
+	print( nrow(subset(df, prefix_hole>0)) )
+	print("Number of Overlap. May be already handled by PLFS")
+	print( nrow(subset(df, prefix_hole<0)) )
+	
+	# IOPS histogram (op issue rate)
+	phist_iops = ggplot(df, aes(Begin_timestamp)) +
+			geom_histogram(aes(y  =  ..count..), binwidth  =  1.0) + ylab("Number of IO ops issued")
+	windows()
+	print(phist_iops)
+	
+	# plot holes of a single pid
+	df.pid = subset(df, ORG.PID == 1)
+	df.pid = find_hole(df.pid)
+	phole_pid = ggplot(df.pid, aes(x=Logical_offset, y=prefix_hole)) +
+			geom_point()
+	windows()
+	print(phole_pid)
+	
+	plength_pid = ggplot(df.pid, aes(x=Logical_offset, y=Length)) +
+			geom_point()
+	windows()
+	print(plength_pid)	
+	
+	# Merge contiguous
+	#df.merged = merge_contiguous(df)
+	#print("number of rows after merging")
+	#print(nrow(df.merged))
+}
+
+############################################
+########## To find hole in each PID group
+############################################
+
+find_hole <- function(map)
+{
+	tail_len = length(map$Logical_tail)
+	map$pre_tail = c(map$Logical_offset[1], map$Logical_tail[1:tail_len-1]+1) 
+	map$prefix_hole = map$Logical_offset - map$pre_tail
+	# If prefix hole is a postive nubmer
+	#    then there is a hole
+	# else if it is negative
+	#    then there is AT LEAST an overlap
+	# else 0
+	#    then it is the first write or it is contiguous with the previous one
+	map
+}
+
+find_hole_bypid <- function(df)
+{
+	ddply(df, .(ORG.PID), find_hole_in_pid)
+}
+
+##############################################
+# df should have 1 row at least
+mark_contiguous_seg <- function(df)
+{
+	df = find_hole(df)
+	n = nrow(df)
+	df$seg = rep(-1, n)
+	segidx = 0
+	for ( i in 1:n) {
+		if (df$prefix_hole != 0 || i == 1) {
+			segidx = segidx + 1
+		}
+		df$seg[i] = segidx
+	}
+	df
+}
+
+# all rows in df should have the same seg number
+shrink_contiguous_seg <- function(df)
+{
+	n = nrow(df)
+	if ( n <= 1 ) {
+		df
+	} else {
+		first = head(df,1)
+		last = tail(df,1)
+		first$Length = sum(as.numeric(df$Length))
+		first$End_timestamp = last$End_timestamp
+		first$Logical_tail = last$Logical_tail
+		first$Chunk_offset = NA
+		first
+	}
 }
 
 
-
-
-
+merge_contiguous <- function(df)
+{
+	df = mark_contiguous_seg(df)
+	ddply(df, .(seg), shrink_contiguous_seg)
+}
 
 
 
